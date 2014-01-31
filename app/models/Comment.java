3cc46fce4bfa1c3b018cabf192e20c34a94d94cd
Chris Barrieau <Chris.Barrieau@hotmail.com>
2014-01-31 14:05:46 -0500

    Finished level 7.

--- app/models/Comment.java
+++ app/models/Comment.java
@@ -4,17 +4,22 @@ import java.util.*;
 import javax.persistence.*;
  
 import play.db.jpa.*;
+import play.data.validation.*;
  
 @Entity
 public class Comment extends Model {
  
+	@Required
     public String author;
+	@Required
     public Date postedAt;
      
     @Lob
+    @Required
     public String content;
     
     @ManyToOne
+    @Required
     public Post post;
     
     public Comment(Post post, String author, String content) {
@@ -23,5 +28,16 @@ public class Comment extends Model {
         this.content = content;
         this.postedAt = new Date();
     }
+    
+    public String toString() {
+    	String message = "";
+    	if (content.length() <= 200) {
+    		message = content;
+    	} //if
+    	else {
+    		message = content.substring(0, 200) + "...";
+    	} //else
+    	return author + " said: " + message + " on " + (postedAt.getYear() + 1900) + "/" + postedAt.getMonth() + "/" + postedAt.getDate();
+    } 
  
 }
